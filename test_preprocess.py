0f2b224791d9af0fc24695053f02a26740683317
Aryan Bhargune <bhargunearyanck12@gmail.com>
2026-03-16 22:43:04 +0530

    Removed generated files and artifacts from repository

--- test_preprocess.py
+++
@@ -1,6 +0,0 @@
-from src.preprocess import preprocess_data
-
-X_train, X_test, y_train, y_test = preprocess_data()
-
-print("Training samples:", len(X_train))
-print("Testing samples:", len(X_test))
